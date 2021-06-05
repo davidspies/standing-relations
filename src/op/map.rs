ffe62d42abe91fc46612adb4cc32389a10eb4587
David Spies <dnspies@gmail.com>
2021-06-05 13:01:13 -0700

    Add the mapping functions

--- src/op/map.rs
+++ src/op/map.rs
@@ -1,4 +1,6 @@
-use crate::Op;
+use std::iter;
+
+use crate::{Op, Relation};
 
 pub struct FlatMap<C: Op, I: IntoIterator, F: Fn(C::T) -> I> {
     inner: C,
@@ -15,5 +17,30 @@ impl<C: Op, I: IntoIterator, F: Fn(C::T) -> I> Op for FlatMap<C, I, F> {
                 continuation(y)
             }
         })
+    }
+}
+
+impl<C: Op> Relation<C> {
+    pub fn flat_map<I: IntoIterator, F: Fn(C::T) -> I>(self, f: F) -> Relation<FlatMap<C, I, F>> {
+        Relation {
+            context_id: self.context_id,
+            dirty: self.dirty,
+            inner: FlatMap {
+                inner: self.inner,
+                f,
+            },
+        }
+    }
+    pub fn map<'a, Y: 'a, F: Fn(C::T) -> Y + 'a>(self, f: F) -> Relation<impl Op<T = Y> + 'a>
+    where
+        C: 'a,
+    {
+        self.flat_map(move |x| iter::once(f(x)))
+    }
+    pub fn filter<'a, F: Fn(&C::T) -> bool + 'a>(self, f: F) -> Relation<impl Op<T = C::T> + 'a>
+    where
+        C: 'a,
+    {
+        self.flat_map(move |x| if f(&x) { Some(x) } else { None })
     }
 }
