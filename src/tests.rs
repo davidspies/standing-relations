afb9bd80b674bf7062256abe38f42368c63bc27c
David Spies <dnspies@gmail.com>
2021-06-04 17:15:53 -0700

    Simple test

--- src/tests.rs
+++ src/tests.rs
@@ -1,4 +1,18 @@
+use std::{collections::HashMap, iter::FromIterator};
+
+use crate::{Context, Output};
+
 #[test]
 fn it_works() {
-    assert_eq!(2 + 2, 4);
+    let mut context = Context::new();
+    let (inp, rel) = context.new_input();
+    let outp: Output<_, _> = rel.get_output();
+    inp.add('a');
+    inp.add('b');
+    inp.add('a');
+    inp.add('b');
+    inp.remove('b');
+    assert_eq!(&*outp.get(), &HashMap::new());
+    context.commit();
+    assert_eq!(&*outp.get(), &HashMap::from_iter(vec![('a', 2), ('b', 1)]));
 }
