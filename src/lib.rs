748ee43be7aee228d7d39165e268a9da4acee2ce
David Spies <dnspies@gmail.com>
2021-06-05 09:45:26 -0700

    Sorting

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,10 @@
+mod concat;
 mod context;
 mod count_map;
 mod dirty;
 mod dynamic;
 mod global_id;
 mod input;
-mod concat;
 mod op;
 mod output;
 mod pipes;
@@ -12,12 +12,12 @@ mod relation;
 mod split;
 mod with_clones;
 
+pub use concat::Concat;
 pub use context::Context;
 pub use count_map::CountMap;
 pub use dynamic::Dynamic;
 pub use input::{Input, InputSender};
 pub use op::Op;
-pub use concat::Concat;
 pub use output::Output;
 pub use relation::Relation;
 pub use split::Split;
