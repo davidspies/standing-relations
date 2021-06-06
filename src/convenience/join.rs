97ab8321ae9e90fd07b1d3a5d3de689014d137eb
David Spies <dnspies@gmail.com>
2021-06-05 17:27:05 -0700

    intersection and set_minus

--- src/convenience/join.rs
+++ src/convenience/join.rs
@@ -10,3 +10,18 @@ impl<K: Clone + Eq + Hash, V: Clone + Eq + Hash, C: Op<T = ((K, V), isize)>> Rel
         self.join(other.map(|x| (x, ()))).map(|(k, v, ())| (k, v))
     }
 }
+
+impl<D: Clone + Eq + Hash, C: Op<T = (D, isize)>> Relation<C> {
+    pub fn intersection<C2: Op<T = (D, isize)>>(
+        self,
+        other: Relation<C2>,
+    ) -> Relation<impl Op<T = (D, isize)>> {
+        self.map(|x| (x, ())).semijoin(other).map(|(x, ())| x)
+    }
+    pub fn set_minus<C2: Op<T = (D, isize)>>(
+        self,
+        other: Relation<C2>,
+    ) -> Relation<impl Op<T = (D, isize)>> {
+        self.map(|x| (x, ())).antijoin(other).map(|(x, ())| x)
+    }
+}
