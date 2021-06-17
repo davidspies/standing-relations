aa4b6c5f93705cb6771a3a144b5c7360621841ac
David Spies <dnspies@gmail.com>
2021-06-17 13:02:09 -0700

    Just send the iterator

--- src/core/op/split.rs
+++ src/core/op/split.rs
@@ -1,35 +1,38 @@
 use crate::core::{
     dirty::DirtyReceive,
+    flat_iter::IntoFlatIterator,
     pipes::{self, Receiver, Sender},
     Op_, Relation,
 };
 use std::{cell::RefCell, rc::Rc};
 
-pub struct Split<T, C: Op_<T = (LI, RI)>, LI: IntoIterator, RI: IntoIterator> {
+pub struct Split<TI: IntoIterator, C: Op_<T = (LI, RI)>, LI: IntoIterator, RI: IntoIterator> {
     inner: Rc<RefCell<SplitInner<C, LI, RI>>>,
-    receiver: Receiver<T>,
+    receiver: Receiver<TI>,
 }
 
 struct SplitInner<C: Op_<T = (LI, RI)>, LI: IntoIterator, RI: IntoIterator> {
     inner: C,
-    left_sender: Sender<LI::Item>,
-    right_sender: Sender<RI::Item>,
+    left_sender: Sender<LI>,
+    right_sender: Sender<RI>,
     dirty: DirtyReceive,
 }
 
-impl<T, C: Op_<T = (LI, RI)>, LI: IntoIterator, RI: IntoIterator> Op_ for Split<T, C, LI, RI> {
-    type T = T;
+impl<TI: IntoIterator, C: Op_<T = (LI, RI)>, LI: IntoIterator, RI: IntoIterator> Op_
+    for Split<TI, C, LI, RI>
+{
+    type T = TI::Item;
 
     fn foreach<'a, F: FnMut(Self::T) + 'a>(&'a mut self, mut continuation: F) {
         if self.inner.borrow().dirty.take_status() {
             let mut inner = self.inner.borrow_mut();
             let data = inner.inner.get_vec();
             for (xl, xr) in data {
-                inner.left_sender.send_all(xl);
-                inner.right_sender.send_all(xr)
+                inner.left_sender.send(xl);
+                inner.right_sender.send(xr)
             }
         }
-        for x in self.receiver.receive() {
+        for x in self.receiver.receive().into_flat_iter() {
             continuation(x)
         }
     }
@@ -39,8 +42,8 @@ impl<C: Op_<T = (LI, RI)>, LI: IntoIterator, RI: IntoIterator> Relation<C> {
     pub fn split_(
         self,
     ) -> (
-        Relation<Split<LI::Item, C, LI, RI>>,
-        Relation<Split<RI::Item, C, LI, RI>>,
+        Relation<Split<LI, C, LI, RI>>,
+        Relation<Split<RI, C, LI, RI>>,
     ) {
         let mut this_dirty = self.dirty.to_receive();
         let left_dirty = this_dirty.add_target();
