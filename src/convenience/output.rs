d279c716c6bc892b462f2d45a907f9ef4bce27ee
David Spies <dnspies@gmail.com>
2021-06-20 09:20:47 -0700

    kv_output

--- src/convenience/output.rs
+++ src/convenience/output.rs
@@ -1,10 +1,23 @@
 use crate::{core::CreationContext, Op, Output, Relation};
-use std::hash::Hash;
+use std::{collections::HashMap, hash::Hash};
 
 impl<C: Op> Relation<C> {
     pub fn get_output(self, context: &CreationContext) -> Output<C::D, C>
     where
         C::D: Eq + Hash,
+    {
+        self.get_output_(context)
+    }
+}
+
+impl<K, V, C: Op<D = (K, V)>> Relation<C> {
+    pub fn get_kv_output(
+        self,
+        context: &CreationContext,
+    ) -> Output<(K, V), C, HashMap<K, HashMap<V, isize>>>
+    where
+        K: Eq + Hash,
+        V: Eq + Hash,
     {
         self.get_output_(context)
     }
