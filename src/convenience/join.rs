40759fe80ffb5e9a141bd4e55d9a8751fa533a16
David Spies <dnspies@gmail.com>
2021-07-13 09:44:08 -0700

    type_named at the end

--- src/convenience/join.rs
+++ src/convenience/join.rs
@@ -4,8 +4,8 @@ use std::hash::Hash;
 impl<K: Clone + Eq + Hash, V: Clone + Eq + Hash, C: Op<D = (K, V)>> Relation<C> {
     pub fn semijoin(self, other: Relation<impl Op<D = K>>) -> Relation<impl Op<D = (K, V)>> {
         self.join(other.map_h(|x| (x, ())))
-            .type_named("semijoin")
             .map_h(|(k, v, ())| (k, v))
+            .type_named("semijoin")
     }
 }
 
@@ -16,13 +16,13 @@ where
     pub fn intersection(self, other: Relation<impl Op<D = C::D>>) -> Relation<impl Op<D = C::D>> {
         self.map_h(|x| (x, ()))
             .semijoin(other)
-            .type_named("intersection")
             .map_h(|(x, ())| x)
+            .type_named("intersection")
     }
     pub fn set_minus(self, other: Relation<impl Op<D = C::D>>) -> Relation<impl Op<D = C::D>> {
         self.map_h(|x| (x, ()))
             .antijoin(other)
-            .type_named("set_minus")
             .map_h(|(x, ())| x)
+            .type_named("set_minus")
     }
 }
